2f4df1b0cb3304134ff31633430fac4959f08d40
baryskhanbimat <baryskhan.bimat@ce.sdu.edu.kz>
2019-09-19 19:02:30 +0600

    1051. Height Checker

---
+++ height-checker.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> temp = heights;
+        sort(temp.begin(),temp.end());
+        int result = 0;
+        for (int i = 0; i < temp.size(); i++) {
+            if (temp[i] != heights[i]) {
+                result++;
+            }
+        }
+        return result;
+    }
+};
